6006e0ed33f8ad0bd77faeaeac7f959eaae3dff6
gtinglinselah <gtinglinselah@ufl.edu>
2023-03-21 12:16:22 -0400

    I added the decoder

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#Richard Romero
+# Richard Romero
 def encode(pw):
     pw_enc = ""
     for num in pw:
         num = int(num) + 3
         pw_enc += str(num)
     return pw_enc
+
+
+# Genesis Selah
+def decode(pw):
+    pw_dec = ""
+    for num in pw:
+        num = int(num) - 3
+        pw_dec += str(num)
+    return pw_dec
 
 
 if __name__ == "__main__":
@@ -19,9 +28,10 @@ if __name__ == "__main__":
 
         if opt == 1:
             pw = input("Please enter your password to encode: ")
-            pw_enc = encode(pw)  
+            pw_enc = encode(pw)
             print(f"Your password has been encoded and stored!")
         elif opt == 2:
-           pass
+            pw_dec = decode(pw_enc)  # decode encoded password and display both
+            print(f'The encoded password is {pw_enc}, and the original password is {pw_dec}.')
         elif opt == 3:
             break
